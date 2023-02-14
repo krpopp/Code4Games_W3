c4dc3c765e95e4613ba5a792213a02538c80075b
Karina <karina.popp@nyu.edu>
2023-02-14 16:10:28 -0500

    Adding TMPro and adding health text

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+
+    public float speed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,20 +19,27 @@ public class PlayerControl : MonoBehaviour
         Vector3 currentPos = transform.position;
         if (Input.GetKey(KeyCode.W))
         {
-            currentPos.z++;
+            //currentPos.z++;
+            currentPos.z = currentPos.z + speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            currentPos.z--;
+            currentPos.z = currentPos.z - speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            currentPos.x--;
+            currentPos.x = currentPos.x - speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            currentPos.x++;
+            currentPos.x = currentPos.x + speed * Time.deltaTime;
         }
-        transform.position = currentPos; ;
+        transform.position = currentPos;
     }
+
+    //int AddSomeNumbers(int a, int b)
+    //{
+    //    int sum = a + b;
+    //    return sum;
+    //}
 }
